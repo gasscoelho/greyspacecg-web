2320211423322e414a12290661e2bfb57cd8bfd2
Gabriel Coelho <gaaucocoelho@gmail.com>
2021-04-12 21:42:23 -0300

    feat: align right in case fa-IR

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,40 @@
 import { Flex, VStack, Text, Container, Divider, Box } from '@chakra-ui/react'
 import { useTranslation } from 'next-i18next'
+import { useRouter } from 'next/router'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Head from 'next/head'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+
+function Title({ title }) {
+  const router = useRouter()
+
+  return (
+    <Text
+      mt="8"
+      textAlign={router.locale === 'fa-IR' ? 'end' : 'start'}
+      fontSize={{ base: 'xl', md: '2xl' }}
+      fontWeight="semibold"
+    >
+      {title}
+    </Text>
+  )
+}
+
+function Description({ desc }) {
+  const router = useRouter()
+  1
+  return (
+    <Text
+      mt="2"
+      fontSize={{ base: 'md', md: 'lg' }}
+      lineHeight="8"
+      textAlign={router.locale === 'fa-IR' ? 'end' : 'justify'}
+    >
+      {desc}
+    </Text>
+  )
+}
 
 export default function About() {
   const { t } = useTranslation('about')
@@ -27,11 +58,7 @@ export default function About() {
         </Flex>
 
         <VStack mt="8" w="full" pb="150px">
-          <Container
-            maxW={{ base: 'container.md', xl: 'container.lg' }}
-            pb="8"
-            textAlign="justify"
-          >
+          <Container maxW={{ base: 'container.md', xl: 'container.lg' }} pb="8">
             <Text
               borderBottom="4px"
               textAlign="center"
@@ -43,59 +70,27 @@ export default function About() {
               {t('page-header')}
             </Text>
 
-            <Text
-              mt="8"
-              textAlign="start"
-              fontSize={{ base: 'xl', md: '2xl' }}
-              fontWeight="semibold"
-            >
-              {t('mission-title')}
-            </Text>
-            <Text mt="2" fontSize={{ base: 'md', md: 'lg' }} lineHeight="8">
-              {t('mission-desc')}
-            </Text>
+            <Title title={t('mission-title')} />
+
+            <Description desc={t('mission-desc')} />
 
             <Divider my="8" />
 
-            <Text
-              mt="8"
-              textAlign="start"
-              fontSize={{ base: 'xl', md: '2xl' }}
-              fontWeight="semibold"
-            >
-              {t('vision-title')}
-            </Text>
-            <Text mt="2" fontSize={{ base: 'md', md: 'lg' }} lineHeight="8">
-              {t('vision-desc')}
-            </Text>
+            <Title title={t('vision-title')} />
+
+            <Description desc={t('vision-desc')} />
 
             <Divider my="8" />
 
-            <Text
-              mt="8"
-              textAlign="start"
-              fontSize={{ base: 'xl', md: '2xl' }}
-              fontWeight="semibold"
-            >
-              {t('values-title')}
-            </Text>
-            <Text mt="2" fontSize={{ base: 'md', md: 'lg' }} lineHeight="8">
-              {t('values-desc')}
-            </Text>
+            <Title title={t('values-title')} />
+
+            <Description desc={t('values-desc')} />
 
             <Divider my="8" />
 
-            <Text
-              mt="8"
-              textAlign="start"
-              fontSize={{ base: 'xl', md: '2xl' }}
-              fontWeight="semibold"
-            >
-              {t('expertise-title')}
-            </Text>
-            <Text mt="2" fontSize={{ base: 'md', md: 'lg' }} lineHeight="8">
-              {t('expertise-desc')}
-            </Text>
+            <Title title={t('expertise-title')} />
+
+            <Description desc={t('expertise-desc')} />
           </Container>
         </VStack>
 
